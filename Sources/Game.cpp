178a02a5934c97216320bddf3836bc9cc2821274
yasu <giants.sy24@gmail.com>
2017-11-28 16:02:23 +0900

    「スコアのサイズを大きくする。(E) スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)　」 やりました。

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -5,8 +5,8 @@
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
-// TODO: スコアのサイズを大きくする。(E)
-// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
+// TODO: スコアのサイズを大きくする。(E) Hw16A100 須賀 康則
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F) HW16A100 須賀 康則
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
 // TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
 
@@ -43,7 +43,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
@@ -67,9 +67,9 @@ void Update()
     // ターゲットの描画
     FillRect(targetRect, Color::red);
 
-    // スコアの描画
-    SetFont("nicoca_v1.ttf", 20.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%02d", score), Vector2(-320, 200), Color::white);
+    // スコアの描画 HW16A100 須賀 康則
+    SetFont("nicoca_v1.ttf", 80.0f);
+    DrawText(FormatString("%05d", score), Vector2(-319, 179), Color::black);
+    DrawText(FormatString("%05d", score), Vector2(-320, 180), Color::white);
 }
 
