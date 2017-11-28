be649e3fb955bf6f5c3781e3f63fc5c2ce16048d
hw15a215 <hw15a215@oecu.jp>
2017-11-28 16:59:53 +0900

    なんかわからんけどコミット

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -5,9 +5,9 @@
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C) 実装：HW15A215 山領萌美
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D) 実装：HW15A215 山領萌美
-// TODO: スコアのサイズを大きくする。(E)
-// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
-// TODO: PlayBGM()関数を使って、BGMを再生する。(G)
+// TODO: スコアのサイズを大きくする。(E) Hw16A100 須賀 康則
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F) HW16A100 須賀 康則
+// TODO: PlayBGM()関数を使って、BGMを再生する。(G) HW16A100 須賀 康則
 // TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
 
 
@@ -28,6 +28,10 @@ void Start()
     bulletPos.x = -999;
     score = 0;
     cannon_y = 1;
+
+    // BGMを再生する。(G) HW16A100 須賀 康則
+    PlayBGM("bgm_maoudamashii_8bit07.mp3");
+    
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -46,7 +50,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
         if(bulletPos.x > 320){
@@ -79,9 +83,9 @@ void Update()
     // ターゲットの描画
     FillRect(targetRect, Color::red);
 
-    // スコアの描画
-    SetFont("nicoca_v1.ttf", 20.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%02d", score), Vector2(-320, 200), Color::white);
+    // スコアの描画 HW16A100 須賀 康則
+    SetFont("nicoca_v1.ttf", 80.0f);
+    DrawText(FormatString("%05d", score), Vector2(-319, 179), Color::black);
+    DrawText(FormatString("%05d", score), Vector2(-320, 180), Color::white);
 }
 
